75a3b497f40f6edb53f103c96ab73191cb27b55b
Ender <endamccormack0@gmail.com>
2015-09-27 23:31:44 +0100

    removed some old stuff from a previous project

--- web-template/app/controllers/application_controller.rb
+++ web-template/app/controllers/application_controller.rb
@@ -1,45 +1,5 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
-
   	def index
 
   	end
-
-	def attempt_login
-		authorized_user = User.authenticate(params[:username], params[:password]) 
-
-		if authorized_user
-		  session[:id] = authorized_user.getEmailAddress
-
-		  session[:username] = authorized_user.getFirstName
-		  session[:companyname] = authorized_user.getCompanyName
-		  session[:companyid] = authorized_user.getCompanyId
-		  #redirect_to(:controller => 'index', :action => 'index')
-		  redirect_to(:controller => 'myaccount', :action => 'dashboard')
-		else
-		  flash[:notice] = "Sorry but your login details did not result in a sucessful login, please try again"
-		  redirect_to(:controller => 'account', :action => 'login')
-		end
-	end
-
-	def logout
-	session[:id] = nil;
-	session[:username] = nil;
-	session[:isAdmin] = nil;
-	flash[:notice] = "You have been sucessfully logged out."
-	  redirect_to(:controller => 'index', :action => 'index')
-	end
-
-	protected
-	def confirm_logged_in
-	unless session[:id]
-	  flash[:notice] = 'Please log in.'
-	  redirect_to(:controller => 'index', :action => 'index')
-	  return false
-	else
-	  return true
-	end
-	end
 end
