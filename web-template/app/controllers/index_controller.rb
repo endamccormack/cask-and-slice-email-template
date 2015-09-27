fc1be228a2c603bbf67f6e15acbee58b317b7d25
Ender <endamccormack0@gmail.com>
2015-09-27 23:36:50 +0100

    changed the home page to generate based on a json file

--- web-template/app/controllers/index_controller.rb
+++ web-template/app/controllers/index_controller.rb
@@ -1,6 +1,26 @@
+require 'pry'
+
 class IndexController < ApplicationController
+
+	@@json_files_location = '/public/events/'
+
 	def index
 	  #render :file => 'public/index.html' and return
-	  render('index')
+		events_directory = Dir.pwd.to_s + @@json_files_location
+
+		if Dir.exists?(events_directory) then
+			@event = Dir.glob(events_directory + "*.json").last
+
+			f = File.open(@event, "r")
+			file_content = f.read()
+			f.close()
+
+			@json = JSON.parse(file_content)
+		end
+
+		@event_date = DateTime.iso8601(@json['date'])
+		@event_talks = @json['talks']
+
+	  render 'index'
 	end
 end
